4d6a7648f6b2ad0a6b1f3ba274ecc894542f85a2
David Chen <davidc3287@gmail.com>
2020-06-22 18:25:06 -0400

    rename files

--- insertion-sort.py
+++
@@ -1,9 +0,0 @@
-'''
-Python implementation of insertion sort
-
-From brilliant.org:
-
-Insertion sort is a comparison-based algorithm that builds a final sorted array one element at a time. 
-It iterates through an input array and removes one element per iteration, finds the place the element 
-belongs in the array, and then places it there.
-'''

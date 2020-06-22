81a099d7ffb00cb6d176ab68abcd8b63d63e577c
David Chen <davidc3287@gmail.com>
2020-06-22 18:47:32 -0400

    finish sort testing and timing code

--- sort_tester.py
+++ sort_tester.py
@@ -2,14 +2,30 @@
 A simple program to time and test various sorting algorithms in Python
 '''
 import timeit
-from . import  *
+import random
 
 
-def test_sort_algorithm(algorithm, array, num_runs):
-    setup = f'from __main__ import {algorithm}'
+def test_sort_algorithm(algorithm):
+    """ Ensure that the provided algorithm correctly sorts arrays """
+    arr = [random.randint(1, 1000) for _ in range(1000)]
+    setup = f'from {algorithm} import {algorithm}'
+    stmt = f'{algorithm}({arr})'
+    print(setup)
+    exec(setup)
+    sorted_arr = exec(stmt)
+    return sorted(arr) == sorted_arr
+
+
+def time_sort_algorithm(algorithm, array, num_runs):
+    setup = f'from {algorithm} import {algorithm}'
     stmt = f'{algorithm}({array})'
-
     time = timeit.timeit(setup=setup, stmt=stmt, number=num_runs)
     return time / num_runs
 
-# test_sort_algorithm('insertion_sort')
+
+if __name__ == '__main__':
+    arr = [random.randint(1, 1000) for _ in range(1000)]
+    print(test_sort_algorithm('insertion_sort'))
+    num_runs = 1000
+    time = time_sort_algorithm('insertion_sort', arr, num_runs)
+    print(f'Time for one run: {time}')

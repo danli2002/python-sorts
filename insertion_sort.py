ff13e031c997aed75c559f8aa13cc4fdde3a7837
David Chen <davidc3287@gmail.com>
2020-06-22 22:03:52 -0400

    finish insertion sort

--- insertion_sort.py
+++ insertion_sort.py
@@ -10,4 +10,13 @@ belongs in the array, and then places it there.
 
 
 def insertion_sort(arr):
+    for i in range(1, len(arr)):
+        curr = arr[i]
+        # shift elements greater than curr to the right, so we can insert curr before them
+        j = i - 1
+        while j >= 0 and curr < arr[j]:
+            arr[j + 1] = arr[j]
+            j -= 1
+        arr[j + 1] = curr  # j + 1 because we decrement j the line above
+
     return arr
